3fdbddeb762fba376f64b3410214460e62e409c5
Sean Baier <sean.t.baier@gmail.com>
2020-07-13 16:00:35 -0500

    adding tf

---
+++ main.tf
@@ -0,0 +1,63 @@
+terraform {
+  backend "remote" {
+    organization = "seanbaier"
+
+    workspaces {
+      name = "fastaoi-serverless-infrastructure"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+provider "template" {
+}
+
+resource "aws_iam_user" "circleci" {
+  name = var.user
+  path = "/system/"
+}
+
+resource "aws_iam_access_key" "circleci" {
+  user = aws_iam_user.circleci.name
+}
+
+data "template_file" "circleci_policy" {
+  template = file("circleci_s3_access.tpl.json")
+  vars = {
+    s3_bucket_arn = aws_s3_bucket.portfolio.arn
+  }
+}
+
+resource "local_file" "circle_credentials" {
+  filename = "tmp/circleci_credentials"
+  content  = "${aws_iam_access_key.circleci.id}\n${aws_iam_access_key.circleci.secret}"
+}
+
+resource "aws_iam_user_policy" "circleci" {
+  name   = "AllowCircleCI"
+  user   = aws_iam_user.circleci.name
+  policy = data.template_file.circleci_policy.rendered
+}
+
+resource "aws_s3_bucket" "portfolio" {
+  tags = {
+    Name = "Portfolio Website Bucket"
+  }
+
+  bucket = "${var.app}.${var.label}"
+  acl    = "public-read"
+
+  website {
+    index_document = "${var.app}.html"
+    error_document = "error.html"
+  }
+  force_destroy = true
+
+}
+
+output "Endpoint" {
+  value = aws_s3_bucket.portfolio.website_endpoint
+}
