ae6637b598ab1e6ef5bcec6f46091813054ad555
Sean Baier <sean.t.baier@gmail.com>
2020-07-13 21:10:42 -0500

    fixing lambda

--- lambda.tf
+++ lambda.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "fastapi_lambda" {
 
   # The bucket name as created earlier with "aws s3api create-bucket"
   s3_bucket = aws_s3_bucket.fastapi_serverless.bucket
-  s3_key    = "v1.0.0/lambda.zip"
+  s3_key    = "v1.0.0/function.zip"
 
   # "main" is the filename within the zip file (main.js) and "handler"
   # is the name of the property under which the handler function was
