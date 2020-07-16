eafa1bf2a500f9a7ed6cc68a819a780bdbebdf5a
Sean Baier <sean.t.baier@gmail.com>
2020-07-16 08:09:01 -0500

    adding logs to api gw

--- api_gateway.tf
+++ api_gateway.tf
@@ -1,3 +1,12 @@
+resource "aws_api_gateway_stage" "fastapi_agw_stage" {
+  depends_on = ["aws_cloudwatch_log_group.fastapi_agw_log_group"]
+  stage_name = "${var.stage}"
+  rest_api_id   = "${aws_api_gateway_rest_api.fastapi_gateway.id}"
+  deployment_id = "${aws_api_gateway_deployment.fastapi_deployment.id}"
+
+  # ... other configuration ...
+}
+
 resource "aws_api_gateway_rest_api" "fastapi_gateway" {
   name        = "fastapi-serverless-api-gateway-${var.stage}"
   description = "FastApi Serverless API Gateway"
@@ -18,6 +27,17 @@ resource "aws_api_gateway_method" "fastapi_method" {
   resource_id   = aws_api_gateway_resource.fastapi_resource.id
   http_method   = "ANY"
   authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_settings" "fastapi_method_settings" {
+  rest_api_id = "${aws_api_gateway_rest_api.fastapi_gateway.id}"
+  stage_name  = "${aws_api_gateway_stage.fastapi_agw_stage.stage_name}"
+  method_path = "${aws_api_gateway_resource.fastapi_resource.path_part}/${aws_api_gateway_method.fastapi_method.http_method}"
+
+  settings {
+    metrics_enabled = true
+    logging_level   = "INFO"
+  }
 }
 
 resource "aws_api_gateway_integration" "fastapi_integration" {
@@ -45,14 +65,6 @@ resource "aws_api_gateway_integration" "fastapi_integration_root" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.fastapi_lambda.invoke_arn
-}
-
-resource "aws_api_gateway_stage" "fastapi_agw_stage" {
-  depends_on = ["aws_cloudwatch_log_group.fastapi_agw_log_group"]
-
-  name = "${var.stage}"
-
-  # ... other configuration ...
 }
 
 resource "aws_api_gateway_deployment" "fastapi_deployment" {
