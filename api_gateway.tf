f6388d288c5f3577dff41625b8f4f0bebbaff8b6
Sean Baier <sean.t.baier@gmail.com>
2020-07-16 07:51:19 -0500

    adding logs to api gw

--- api_gateway.tf
+++ api_gateway.tf
@@ -6,3 +6,8 @@ resource "aws_api_gateway_rest_api" "fastapi_gateway" {
 output "base_url" {
   value = aws_api_gateway_deployment.fastapi_deployment.invoke_url
 }
+
+resource "aws_cloudwatch_log_group" "fastapi_gateway_log_group" {
+  name              = "API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.fastapi_gateway.id}/${var.stage}"
+  retention_in_days = 7
+}
