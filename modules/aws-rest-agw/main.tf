8b6697dc3b2ca1717b07f2b996e0b243bbfdb315
Sean Baier <sean.t.baier@gmail.com>
2020-07-25 15:42:36 -0500

    change

--- modules/aws-rest-agw/main.tf
+++ modules/aws-rest-agw/main.tf
@@ -45,23 +45,6 @@ resource "aws_api_gateway_integration" "fastapi_integration" {
   uri                     = var.invoke_arn
 }
 
-resource "aws_api_gateway_method" "fastapi_proxy_root" {
-  rest_api_id   = aws_api_gateway_rest_api.fastapi_gateway.id
-  resource_id   = aws_api_gateway_rest_api.fastapi_gateway.root_resource_id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "fastapi_integration_root" {
-  rest_api_id = aws_api_gateway_rest_api.fastapi_gateway.id
-  resource_id = aws_api_gateway_method.fastapi_proxy_root.resource_id
-  http_method = aws_api_gateway_method.fastapi_proxy_root.http_method
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = var.invoke_arn
-}
-
 resource "aws_api_gateway_deployment" "fastapi_deployment" {
   depends_on = [
     aws_api_gateway_integration.fastapi_integration,
